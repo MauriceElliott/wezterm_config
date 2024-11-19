907dd600d85f15e62ed119c4f9e113d4c0646c74
MauriceElliott <mauricemelliott@gmail.com>
2024-11-19 23:27:05 +0000

    Added keybindings for changing focused pane:

--- wezterm.lua
+++ wezterm.lua
@@ -1,31 +1,31 @@
   -- Pull in the wezterm API
-  local wezterm = require 'wezterm'
-  local config = wezterm.config_builder()
-  local is_macos = wezterm.target_triple:find("darwin") ~= nil
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+local is_macos = wezterm.target_triple:find("darwin") ~= nil
 
-  if is_macos == false then
-      config.default_prog = { 'ubuntu' }
-  end
-  -- This will hold the configuration.
-  config.window_background_opacity = 0.67
-  config.macos_window_background_blur = 20
-  
-  -- This is where you actually apply your config choice
-  config.font_size = 14.5
-  -- For example, changing the color scheme:
-  config.color_scheme = 'kanagawabones'
-  config.window_decorations = "RESIZE"
-  config.hide_tab_bar_if_only_one_tab = true
-  local act = wezterm.action
-  -- config.leader = { key = 'a', mods = 'ALT|SHIFT' }
-  config.keys = {
+if is_macos == false then
+    config.default_prog = { 'ubuntu' }
+end
+-- This will hold the configuration.
+config.window_background_opacity = 0.67
+config.macos_window_background_blur = 20
+
+-- This is where you actually apply your config choice
+config.font_size = 14.5
+-- For example, changing the color scheme:
+config.color_scheme = 'kanagawabones'
+config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
+local act = wezterm.action
+
+config.keys = {
   {
       key = '9',
       mods = 'CTRL',
       action = act.PaneSelect {
         alphabet = '1234567890',
       }
-    },
+  },
   {
       key = 'LeftArrow',
       mods = 'ALT|SHIFT',
@@ -45,7 +45,27 @@
       key = 'RightArrow',
       mods = 'ALT|SHIFT',
       action = act.AdjustPaneSize { 'Right', 5 },
-      },
-    }
+  },
+  {
+    key = 'LeftArrow',
+    mods = 'CTRL|SHIFT',
+    action = act.ActivatePaneDirection 'Left',
+  },
+  {
+    key = 'RightArrow',
+    mods = 'CTRL|SHIFT',
+    action = act.ActivatePaneDirection 'Right',
+  },
+  {
+    key = 'UpArrow',
+    mods = 'CTRL|SHIFT',
+    action = act.ActivatePaneDirection 'Up',
+  },
+  {
+    key = 'DownArrow',
+    mods = 'CTRL|SHIFT',
+    action = act.ActivatePaneDirection 'Down',
+  },
+}
 
-  return config
+return config
